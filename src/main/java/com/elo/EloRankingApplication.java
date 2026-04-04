6a4b7b394910631325af1bb4a0f87709290dca1a
Guillaume Lerda <lerda.guillaume@gmail.com>
2026-04-04 02:23:17 +0200

    Add startup log

--- src/main/java/com/elo/EloRankingApplication.java
+++ src/main/java/com/elo/EloRankingApplication.java
@@ -1,12 +1,21 @@
 package com.elo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EloRankingApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(EloRankingApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(EloRankingApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(EloRankingApplication.class, args);
+        Environment env = context.getEnvironment();
+        String port = env.getProperty("server.port", "8080");
+        log.info("Swagger UI: http://localhost:{}/swagger-ui/index.html", port);
     }
 }
